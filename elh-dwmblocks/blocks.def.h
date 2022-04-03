8ebfb0efe33cece513b594d1ac217395583d3c0f
Ellie Helisky <92484654+elliehelisky@users.noreply.github.com>
2022-04-03 23:29:44 +0000

    Add files via upload

---
+++ elh-dwmblocks/blocks.def.h
@@ -0,0 +1,16 @@
+// Modify this file to change what commands output to your statusbar, and
+// recompile using the make command.
+static const Block blocks[] = {
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    {"Updates: ",
+     "sudo xbps-install -S > /dev/null 2>&1; ~/.local/bin/xbps-updates", 30, 0},
+    {"󰕾 : ", "pamixer --get-volume", 1, 0},
+    {"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
+    {"", "date '+%b %d (%a) %I:%M%p'", 5, 0},
+
+};
+
+// sets delimeter between status commands. NULL character ('\0') means no
+// delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
