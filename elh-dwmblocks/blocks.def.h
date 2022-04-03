b483cd55c81b4d3b415f61ba46a7b0d212b9a05f
Ellie Helisky <92484654+elliehelisky@users.noreply.github.com>
2022-04-03 23:35:37 +0000

    Update blocks.def.h

--- elh-dwmblocks/blocks.def.h
+++ elh-dwmblocks/blocks.def.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
     {"Updates: ",
      "sudo xbps-install -S > /dev/null 2>&1; ~/.local/bin/xbps-updates", 30, 0},
-    {"󰕾 : ", "pamixer --get-volume", 1, 0},
+    {"Vol : ", "pamixer --get-volume", 1, 0},
     {"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
     {"", "date '+%b %d (%a) %I:%M%p'", 5, 0},
 
