752ca9e1386794b6b09a72b4aa503bfb50f733b8
Ellie <92484654+elliehelisky@users.noreply.github.com>
2022-06-04 04:18:52 +0000

    Replaced Icons with FontAwesome5 Icons

--- elh-dwmblocks/blocks.def.h
+++ elh-dwmblocks/blocks.def.h
@@ -2,13 +2,13 @@
 // recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-    {"Updates: ",
-     "sudo xbps-install -S > /dev/null 2>&1; ~/.local/bin/xbps-updates", 30, 0},
-    {"Vol : ", "pamixer --get-volume", 1, 0},
-    {"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
+    {" : ", "sudo xbps-install -S > /dev/null 2>&1; ~/.local/bin/xbps-updates",
+     30, 0},
+    {" : ", "pamixer --get-volume", 1, 0},
+    {" ", "free -h | awk '/^Mem/ { print $3\"/\" $2 }' | sed s/i//g", 30, 0},
     {"", "date '+%b %d (%a) %I:%M%p'", 5, 0},
-    {"Void Linux: ", "uname -r", 0, 0},
-    
+    /*  {"Kernel: ", "uname -r", 0, 0}, */
+
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no
